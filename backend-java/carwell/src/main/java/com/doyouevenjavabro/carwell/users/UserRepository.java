db49f594dfeee29df044007e831790fab02b2bcf
Unknown <4nikmel@gmail.com>
2019-04-10 18:23:23 -0700

    Backend java Classes setup in progress

---
+++ backend-java/carwell/src/main/java/com/doyouevenjavabro/carwell/users/UserRepository.java
@@ -0,0 +1,7 @@
+package com.doyouevenjavabro.carwell.users;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository <User, String> {
+
+}
