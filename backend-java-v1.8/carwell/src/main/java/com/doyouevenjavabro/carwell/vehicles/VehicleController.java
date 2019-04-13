2f4d59984d5ad5103857a9bdd83a0d9dc3b73744
Austin Flowers <austinflowers1944@yahoo.com>
2019-04-12 21:39:20 -0400

    Finished testing working React API FRONT-END

--- backend-java-v1.8/carwell/src/main/java/com/doyouevenjavabro/carwell/vehicles/VehicleController.java
+++ backend-java-v1.8/carwell/src/main/java/com/doyouevenjavabro/carwell/vehicles/VehicleController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class VehicleController {
 	VehicleRepository vehicleDB;
 
 //	GET all vehicles @ api/vehicles/all
+	@CrossOrigin(origins = "http://localhost:3001")
 	@GetMapping("vehicles/all")
 	public List<Vehicle> getVehicles() {
 		System.out.println("Got request for all vehicles.");
--- frontend-react/carwell-app/src/App.js
+++ frontend-react/carwell-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Vehicles from './components/Vehicles';
+import Vehicles from './components/vehicles';
 
 class App extends Component {
     render() {
