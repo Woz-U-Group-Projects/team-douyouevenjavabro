a556968545c97baea42476c6daf3f39ea9f5505b
rtj-meanstack <Rafael.Torres@my.scitexas.edu>
2019-04-12 21:51:48 -0700

    Put mapping for the vehicles is working.

--- backend-java-v1.8/carwell/src/main/java/com/doyouevenjavabro/carwell/vehicles/VehicleController.java
+++ backend-java-v1.8/carwell/src/main/java/com/doyouevenjavabro/carwell/vehicles/VehicleController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -81,4 +83,16 @@ public class VehicleController {
 		}
 		return ResponseEntity.ok(foundVehicle);
 	}
+	
+	//	POST vehicle (id)
+	@PostMapping("/vehicles")
+	public ResponseEntity<Vehicle> postVehicle(@RequestBody Vehicle vehicle) {
+		Vehicle createdVehicle = vehicleDB.save(vehicle);
+		return ResponseEntity.ok(createdVehicle);
+	}
+	
+	//	PUT vehicle (id)
+		
+	//	DELETE vehicle (id)
+	
 }
