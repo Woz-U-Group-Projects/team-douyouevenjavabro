5ef524a326b06a4840232e610fc67eefde16cb24
rtj-meanstack <Rafael.Torres@my.scitexas.edu>
2019-04-14 13:05:20 -0700

    added ServiceMaster
    CherryPicked and merged Nikos' files with mine.

--- backend-java-v1.8/carwell/src/main/java/com/doyouevenjavabro/carwell/CarwellApplication.java
+++ backend-java-v1.8/carwell/src/main/java/com/doyouevenjavabro/carwell/CarwellApplication.java
@@ -3,12 +3,16 @@ package com.doyouevenjavabro.carwell;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//import com.doyouevenjavabro.carwell.services.ServiceMaster;
+
 @SpringBootApplication
 public class CarwellApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarwellApplication.class, args);
-		System.out.println("It is fun to be a part of DoYouEvenJavaBro group!");
+		System.out.println("bleh");
+//		System.out.println(ServiceMaster.milestone90k);
+		
 	}
 
 }
--- backend-java-v1.8/carwell/src/main/java/com/doyouevenjavabro/carwell/vehicles/VehicleController.java
+++ backend-java-v1.8/carwell/src/main/java/com/doyouevenjavabro/carwell/vehicles/VehicleController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class VehicleController {
