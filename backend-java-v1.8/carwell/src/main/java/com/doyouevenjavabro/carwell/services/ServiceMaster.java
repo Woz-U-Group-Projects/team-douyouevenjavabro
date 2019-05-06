1465138f592988614ecb0bfdf7775ce143a4f255
4Nikos <4nikmel@gmail.com>
2019-05-05 18:54:39 -0700

    merging backend

---
+++ backend-java-v1.8/carwell/src/main/java/com/doyouevenjavabro/carwell/services/ServiceMaster.java
@@ -0,0 +1,122 @@
+package com.doyouevenjavabro.carwell.services;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.doyouevenjavabro.carwell.vehicles.Vehicle;
+
+public class ServiceMaster {
+//	this class contains hard coded values of different services
+
+//A	
+	public final static String serviceA1 = "Replace engine oil!";
+	public final static String serviceA2 = "Rotate tires!";
+
+//B	
+	public final static String serviceB1 = "Replace engine oil filter!";
+	public final static String serviceB2 = "Inspect front and rear brakes!";
+	public final static String serviceB3 = "Check parking brake adjustment!";
+	public final static String serviceB4 = "Inspect tie rod ends, steering gearbox and boots!";
+	public final static String serviceB5 = "Inspect suspension components!";
+	public final static String serviceB6 = "Inspect driveshaft boots!";
+	public final static String serviceB7 = "Inspect brake hoses and lines!";
+	public final static String serviceB8 = "Check all fluid levels, condition of fluids, and check for leaks!";
+	public final static String serviceB9 = "Inspect cooling system hoses and connections!";
+	public final static String serviceB10 = "Inspect exhaust system!";
+	public final static String serviceB11 = "Inspect fuel lines and connections!";
+
+//C
+	public final static String serviceC1 = "RepIace air cleaner element!";
+	public final static String serviceC2 = "Replace spark plugs!";
+	public final static String serviceC3 = "Inspect and adjust drive belts!";
+
+//Add-ons
+	public final static String serviceAdd30k = "Check valve clearence, cold engine!";
+	public final static String serviceAdd45k = "Replace coolant! Replace break fluid!";
+	public final static String serviceAdd75k = "Replace coolant!";
+	public final static String serviceAdd90k = "Replace break fluid! Replace transmission fluid!";
+	public final static String serviceAdd105k = "Replace timing belt, inspect water pump! Replace coolant! Check idle speed!";
+	public final static String serviceFinal = "I got no recommendations from the dealer for the cars with mileage beyond 150,000 miles, maybe it is a time to think about a new car??";
+//milestones
+	// 7500
+	public final static String[] milestone7hk = { "Recommendations for 7500 miles milestone", serviceA1, serviceA2 };
+	// 15000
+	public final static String[] milestone15k = { "Recommendations for 15000 miles milestone", serviceA1, serviceA2, serviceB1, serviceB2, serviceB3, serviceB4,
+			serviceB5, serviceB6, serviceB7, serviceB8, serviceB9, serviceB10, serviceB11 };
+	// 22500
+	public final static String[] milestone22hk = { "Recommendations for 22500 miles milestone", serviceA1, serviceA2 };
+	// 30000
+	public final static String[] milestone30k = { "Recommendations for 30000 miles milestone", serviceA1, serviceA2, serviceB1, serviceB2, serviceB3, serviceB4,
+			serviceB5, serviceB6, serviceB7, serviceB8, serviceB9, serviceB10, serviceB11, serviceC1, serviceC2,
+			serviceC3, serviceAdd30k };
+	// 37500
+	public final static String[] milestone37hk = { "Recommendations for 37500 miles milestone", serviceA1, serviceA2 };
+	// 45000
+	public final static String[] milestone45k = { "Recommendations for 45000 miles milestone", serviceA1, serviceA2, serviceB1, serviceB2, serviceB3, serviceB4,
+			serviceB5, serviceB6, serviceB7, serviceB8, serviceB9, serviceB10, serviceB11, serviceAdd45k };
+	// 52500
+	public final static String[] milestone52hk = { "Recommendations for 52500 miles milestone", serviceA1, serviceA2 };
+	// 60000
+	public final static String[] milestone60k = { "Recommendations for 60000 miles milestone", serviceA1, serviceA2, serviceB1, serviceB2, serviceB3, serviceB4,
+			serviceB5, serviceB6, serviceB7, serviceB8, serviceB9, serviceB10, serviceB11, serviceC1, serviceC2,
+			serviceC3 };
+	// 67500
+	public final static String[] milestone67hk = { "Recommendations for 67500 miles milestone", serviceA1, serviceA2 };
+	// 75000
+	public final static String[] milestone75k = { "Recommendations for 75000 miles milestone", serviceA1, serviceA2, serviceB1, serviceB2, serviceB3, serviceB4,
+			serviceB5, serviceB6, serviceB7, serviceB8, serviceB9, serviceB10, serviceB11, serviceAdd75k };
+	// 82500
+	public final static String[] milestone82hk = {"Recommendations for 82500 miles milestone",  serviceA1, serviceA2 };
+	// 90000
+	public final static String[] milestone90k = { "Recommendations for 90000 miles milestone", serviceA1, serviceA2, serviceB1, serviceB2, serviceB3, serviceB4,
+			serviceB5, serviceB6, serviceB7, serviceB8, serviceB9, serviceB10, serviceB11, serviceC1, serviceC2,
+			serviceC3, serviceAdd90k };
+	// 97500
+	public final static String[] milestone97hk = { "Recommendations for 97500 miles milestone", serviceA1, serviceA2 };
+	// 105000
+	public final static String[] milestone105k = { "Recommendations for 105000 miles milestone", serviceA1, serviceA2, serviceB1, serviceB2, serviceB3, serviceB4,
+			serviceB5, serviceB6, serviceB7, serviceB8, serviceB9, serviceB10, serviceB11, serviceAdd105k };
+	// 112500
+	public final static String[] milestone112hk = { "Recommendations for 112500 miles milestone", serviceA1, serviceA2 };
+	// 120000
+	public final static String[] milestone120k = { "Recommendations for 120000 miles milestone", serviceA1, serviceA2, serviceB1, serviceB2, serviceB3, serviceB4,
+			serviceB5, serviceB6, serviceB7, serviceB8, serviceB9, serviceB10, serviceB11, serviceC1, serviceC2,
+			serviceC3 };
+	// 127500
+	public final static String[] milestone127hk = { "Recommendations for 127500 miles milestone", serviceA1, serviceA2 };
+	// 135000
+	public final static String[] milestone135k = { "Recommendations for 135000 miles milestone", serviceA1, serviceA2, serviceB1, serviceB2, serviceB3, serviceB4,
+			serviceB5, serviceB6, serviceB7, serviceB8, serviceB9, serviceB10, serviceB11 };
+	// 142500
+	public final static String[] milestone142hk = { "Recommendations for 142500 miles milestone", serviceA1, serviceA2 };
+	// 150000
+	public final static String[] milestone150k = { "Recommendations for 150000 miles milestone", serviceA1, serviceA2, serviceB1, serviceB2, serviceB3, serviceB4,
+			serviceB5, serviceB6, serviceB7, serviceB8, serviceB9, serviceB10, serviceB11, serviceC1, serviceC2,
+			serviceC3 };
+	// Above150000
+	public final static String[] milestoneAbove150k = { serviceFinal };
+	// all
+	public final static String[][] allMilestones = { milestone7hk, milestone15k, milestone22hk, milestone30k,
+			milestone37hk, milestone45k, milestone52hk, milestone60k, milestone67hk, milestone75k, milestone82hk,
+			milestone90k, milestone97hk, milestone105k, milestone112hk, milestone120k, milestone127hk, milestone135k,
+			milestone142hk, milestone150k, milestoneAbove150k };
+
+////method to calculate difference in days
+	public static long dayDifference(String regDate) throws ParseException {
+		String currentDate = Vehicle.setTimeStamp();
+		SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+
+		Date regDateParsed = format.parse(regDate);
+		Date currentDateParsed = format.parse(currentDate);
+		long diff = currentDateParsed.getTime() - regDateParsed.getTime();
+		long diffDays = diff / (24 * 60 * 60 * 1000);
+		return diffDays;
+	}
+	
+////method to calculate current mileage
+	public static Integer currentMileage(Integer regMiles, String regDate, String currentDate, Integer milesPerDay) {
+		
+		return milesPerDay;
+	}
+}
---
+++ backend-java-v1.8/carwell/src/main/java/com/doyouevenjavabro/carwell/services/ServiceMasterController.java
@@ -0,0 +1,314 @@
+package com.doyouevenjavabro.carwell.services;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.doyouevenjavabro.carwell.vehicles.Vehicle;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/servicemaster")
+public class ServiceMasterController {
+
+	// GET all
+	@RequestMapping(method = RequestMethod.GET, value = { "", "/" })
+	public String[][] allMilestones() {
+
+		return ServiceMaster.allMilestones;
+	}
+
+	// GET 7500
+	@RequestMapping(method = RequestMethod.GET, value = { "7500", "7500/" })
+	public String[] get7500() {
+
+		return ServiceMaster.milestone7hk;
+	}
+
+	// GET 15000
+	@RequestMapping(method = RequestMethod.GET, value = { "15000", "15000/" })
+	public String[] get15000() {
+
+		return ServiceMaster.milestone15k;
+	}
+
+	// GET 22500
+	@RequestMapping(method = RequestMethod.GET, value = { "22500", "22500/" })
+	public String[] get22500() {
+
+		return ServiceMaster.milestone22hk;
+	}
+
+	// GET 30000
+	@RequestMapping(method = RequestMethod.GET, value = { "30000", "30000/" })
+	public String[] get30000() {
+
+		return ServiceMaster.milestone30k;
+	}
+
+	// GET 37500
+	@RequestMapping(method = RequestMethod.GET, value = { "37500", "37500/" })
+	public String[] get37500() {
+
+		return ServiceMaster.milestone37hk;
+	}
+
+	// GET 45000
+	@RequestMapping(method = RequestMethod.GET, value = { "45000", "45000/" })
+	public String[] get45000() {
+
+		return ServiceMaster.milestone45k;
+	}
+
+	// GET 52500
+	@RequestMapping(method = RequestMethod.GET, value = { "52500", "52500/" })
+	public String[] get52500() {
+
+		return ServiceMaster.milestone52hk;
+	}
+
+	// GET 60000
+	@RequestMapping(method = RequestMethod.GET, value = { "60000", "60000/" })
+	public String[] get60000() {
+
+		return ServiceMaster.milestone60k;
+	}
+
+	// GET 67500
+	@RequestMapping(method = RequestMethod.GET, value = { "67500", "67500/" })
+	public String[] get67500() {
+
+		return ServiceMaster.milestone67hk;
+	}
+
+	// GET 75000
+	@RequestMapping(method = RequestMethod.GET, value = { "75000", "75000/" })
+	public String[] get75000() {
+
+		return ServiceMaster.milestone75k;
+	}
+
+	// GET 82500
+	@RequestMapping(method = RequestMethod.GET, value = { "82500", "82500/" })
+	public String[] get82500() {
+
+		return ServiceMaster.milestone82hk;
+	}
+
+	// GET 90000
+	@RequestMapping(method = RequestMethod.GET, value = { "90000", "90000/" })
+	public String[] get90000() {
+
+		return ServiceMaster.milestone90k;
+	}
+
+	// GET 97500
+	@RequestMapping(method = RequestMethod.GET, value = { "97500", "97500/" })
+	public String[] get97500() {
+
+		return ServiceMaster.milestone97hk;
+	}
+
+	// GET 105000
+	@RequestMapping(method = RequestMethod.GET, value = { "105000", "105000/" })
+	public String[] get105000() {
+
+		return ServiceMaster.milestone105k;
+	}
+
+	// GET 112500
+	@RequestMapping(method = RequestMethod.GET, value = { "112500", "112500/" })
+	public String[] get112500() {
+
+		return ServiceMaster.milestone112hk;
+	}
+
+	// GET 120000
+	@RequestMapping(method = RequestMethod.GET, value = { "120000", "120000/" })
+	public String[] get120000() {
+
+		return ServiceMaster.milestone120k;
+	}
+
+	// GET 127500
+	@RequestMapping(method = RequestMethod.GET, value = { "127500", "127500/" })
+	public String[] get127500() {
+
+		return ServiceMaster.milestone127hk;
+	}
+
+	// GET 135000
+	@RequestMapping(method = RequestMethod.GET, value = { "135000", "135000/" })
+	public String[] get135000() {
+
+		return ServiceMaster.milestone135k;
+	}
+
+	// GET 142500
+	@RequestMapping(method = RequestMethod.GET, value = { "142500", "142500/" })
+	public String[] get142500() {
+
+		return ServiceMaster.milestone142hk;
+	}
+
+	// GET 150000
+	@RequestMapping(method = RequestMethod.GET, value = { "150000", "150000/" })
+	public String[] get150000() {
+
+		return ServiceMaster.milestone150k;
+	}
+
+	// GET Above 150000 miles
+	@RequestMapping(method = RequestMethod.GET, value = { "final", "final/" })
+	public String[] getFinal() {
+
+		return ServiceMaster.milestoneAbove150k;
+	}
+
+//GET by Special Calculation
+//needed: milesAtRegestration, currentDate, redDate, milesPerDay
+	@RequestMapping(method = RequestMethod.GET, value = "reminder/{milesAtRegestration}/{redDate}/{milesPerDay}")
+
+	public String[] path(@PathVariable("milesAtRegestration") Integer milesAtRegestration,
+			@PathVariable("redDate") String regDate, @PathVariable("milesPerDay") Integer milesPerDay)
+
+			throws ParseException {
+		// get current date
+		String currentDate = Vehicle.setTimeStamp();
+		// calculate difference in days
+		SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+
+		Date regDateParsed = format.parse(regDate);
+		Date currentDateParsed = format.parse(currentDate);
+		long diff = currentDateParsed.getTime() - regDateParsed.getTime();
+		long diffDays = diff / (24 * 60 * 60 * 1000);
+
+		Integer diffDaysInt = (int) diffDays;
+		// calculate current mileage
+		Integer currentMileage = milesPerDay * (diffDaysInt) + milesAtRegestration;
+
+		System.out.println("Got call for a custom API with (Miles At Registrations: " + milesAtRegestration
+				+ "; Registration Date: " + regDate + "; Miles Per Day: " + milesPerDay);
+		System.out.println("Today: " + currentDate);
+		System.out.println("Days passed since vehicle was registered: " + diffDays);
+		System.out.println("Taking all data into consideration vehicle should have about " + currentMileage + " miles");
+
+		String[] usersRecomendation = {"There was an error"};
+		// 7500 miles
+		if (currentMileage >= 0 && currentMileage <= 7500) {
+			System.out.println("Next milestone is 7500");
+			usersRecomendation = ServiceMaster.milestone7hk;
+		}
+		// 15000 miles
+		else if (currentMileage >= 7501 && currentMileage <= 15000) {
+			System.out.println("Next milestone is 15000");
+			usersRecomendation = ServiceMaster.milestone15k;
+		}
+		// 22500 miles
+		else if (currentMileage >= 15001 && currentMileage <= 22500) {
+			System.out.println("Next milestone is 22500");
+			usersRecomendation = ServiceMaster.milestone22hk;
+		}
+		// 30000 miles
+		else if (currentMileage >= 22501 && currentMileage <= 30000) {
+			System.out.println("Next milestone is 30000");
+			usersRecomendation = ServiceMaster.milestone30k;
+		}
+		// 37500 miles
+		else if (currentMileage >= 30001 && currentMileage <= 37500) {
+			System.out.println("Next milestone is 37500");
+			usersRecomendation = ServiceMaster.milestone37hk;
+		}
+		// 45000 miles
+		else if (currentMileage >= 37501 && currentMileage <= 45000) {
+			System.out.println("Next milestone is 45000");
+			usersRecomendation = ServiceMaster.milestone45k;
+		}
+		// 52500 miles
+		else if (currentMileage >= 45001 && currentMileage <= 52500) {
+			System.out.println("Next milestone is 52500");
+			usersRecomendation = ServiceMaster.milestone52hk;
+		}
+		// 60000 miles
+		else if (currentMileage >= 52501 && currentMileage <= 60000) {
+			System.out.println("Next milestone is 60000");
+			usersRecomendation = ServiceMaster.milestone60k;
+		}
+		// 67500 miles
+		else if (currentMileage >= 60001 && currentMileage <= 67500) {
+			System.out.println("Next milestone is 67500");
+			usersRecomendation = ServiceMaster.milestone67hk;
+		}
+		// 75000 miles
+		else if (currentMileage >= 67501 && currentMileage <= 75000) {
+			System.out.println("Next milestone is 75000");
+			usersRecomendation = ServiceMaster.milestone75k;
+		}
+		// 82500 miles
+		else if (currentMileage >= 75001 && currentMileage <= 82500) {
+			System.out.println("Next milestone is 82500");
+			usersRecomendation = ServiceMaster.milestone82hk;
+		}
+		// 90000 miles
+		else if (currentMileage >= 82501 && currentMileage <= 90000) {
+			System.out.println("Next milestone is 90000");
+			usersRecomendation = ServiceMaster.milestone90k;
+		}
+		// 97500 miles
+		else if (currentMileage >= 90001 && currentMileage <= 97500) {
+			System.out.println("Next milestone is 97500");
+			usersRecomendation = ServiceMaster.milestone97hk;
+		}
+		// 105000 miles
+		else if (currentMileage >= 97501 && currentMileage <= 105000) {
+			System.out.println("Next milestone is 105000");
+			usersRecomendation = ServiceMaster.milestone105k;
+		}
+		// 112500 miles
+		else if (currentMileage >= 105001 && currentMileage <= 112500) {
+			System.out.println("Next milestone is 112500");
+			usersRecomendation = ServiceMaster.milestone112hk;
+		}
+		// 120000 miles
+		else if (currentMileage >= 112501 && currentMileage <= 120000) {
+			System.out.println("Next milestone is 120000");
+			usersRecomendation = ServiceMaster.milestone120k;
+		}
+		// 127500 miles
+		else if (currentMileage >= 120001 && currentMileage <= 127500) {
+			System.out.println("Next milestone is 127500");
+			usersRecomendation = ServiceMaster.milestone127hk;
+		}
+		// 135000 miles
+		else if (currentMileage >= 127501 && currentMileage <= 135000) {
+			System.out.println("Next milestone is 135000");
+			usersRecomendation = ServiceMaster.milestone135k;
+		}
+		// 142500 miles
+		else if (currentMileage >= 135001 && currentMileage <= 142500) {
+			System.out.println("Next milestone is 142500");
+			usersRecomendation = ServiceMaster.milestone142hk;
+		}
+		// 150000 miles
+		else if (currentMileage >= 142501 && currentMileage <= 150000) {
+			System.out.println("Next milestone is 150000");
+			usersRecomendation = ServiceMaster.milestone150k;
+		}
+		// Above 150000 miles
+		else if (currentMileage >= 150001) {
+			System.out.println("Above 15000 miles, no recomendations");
+			usersRecomendation = ServiceMaster.milestoneAbove150k;
+		} else {
+			System.out.println("There was an error in a calculation");
+		}
+
+		return usersRecomendation;
+
+	}
+}
